c059684264cdcbf5f90b8128bf975fa8ca6a053a
LeonNepelius <leon@nepelius.at>
2020-10-26 11:03:21 +0100

    Vector reading support

--- PrimeWPF/PMDL.cs
+++ PrimeWPF/PMDL.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using Aspose.ThreeD.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace PrimeWPF
 {
     class PMDL : Tag
     {
+        public uint Unknown { get; set; }
+        public uint Count { get; set; }
+        public Vector4 Vector4Offset { get; set; }
         public PMDL() : base()
         {
-
+            Unknown = TRB._f.ReadUInt32();
+            Count = TRB._f.ReadUInt32();
+            Vector4Offset = ReadHelper.ReadVector4FromOffset(TRB._f.ReadUInt32() + TRB.sections[1].SectionOffset);
+            Console.WriteLine();
         }
     }
 }
--- PrimeWPF/ReadHelper.cs
+++ PrimeWPF/ReadHelper.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using Aspose.ThreeD.Utilities;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace PrimeWPF
@@ -24,6 +28,44 @@ namespace PrimeWPF
             var str = ReadString();
             TRB._f.BaseStream.Seek(pos, SeekOrigin.Begin);
             return str;
+        }
+
+        public static byte[] ReadFromOffset(int bytesToRead, uint offset)
+        {
+            var pos = TRB._f.BaseStream.Position;
+            TRB._f.BaseStream.Seek(offset, SeekOrigin.Begin);
+            var buffer = new byte[bytesToRead];
+            var bytesRead = TRB._f.Read(buffer);
+            TRB._f.BaseStream.Seek(pos, SeekOrigin.Begin);
+            return buffer;
+        }
+
+        public static long SeekToOffset(uint offset)
+        {
+            var pos = TRB._f.BaseStream.Position;
+            TRB._f.BaseStream.Seek(offset, SeekOrigin.Begin);
+            return pos;
+        }
+
+        public static Vector4 ReadVector4() => new Vector4(TRB._f.ReadSingle(), TRB._f.ReadSingle(), TRB._f.ReadSingle(), TRB._f.ReadSingle());
+        public static Vector3 ReadVector3() => new Vector3(TRB._f.ReadSingle(), TRB._f.ReadSingle(), TRB._f.ReadSingle());
+
+        public static Vector4 ReadVector4FromOffset(uint offset)
+        {
+            var pos = TRB._f.BaseStream.Position;
+            TRB._f.BaseStream.Seek(offset, SeekOrigin.Begin);
+            var result = ReadVector4();
+            TRB._f.BaseStream.Seek(pos, SeekOrigin.Begin);
+            return result;
+        }
+
+        public static Vector3 ReadVector3FromOffset(uint offset)
+        {
+            var pos = TRB._f.BaseStream.Position;
+            TRB._f.BaseStream.Seek(offset, SeekOrigin.Begin);
+            var result = ReadVector3();
+            TRB._f.BaseStream.Seek(pos, SeekOrigin.Begin);
+            return result;
         }
     }
 }
